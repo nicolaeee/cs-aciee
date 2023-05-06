cfa10c92e716457947d3e30d8cfc0b5f3072b2ad
onyxgeek <onyxpeek@gmail.com>
2023-05-06 21:13:02 +0300

    feat: finish the particles component and header

---
+++ src/components/ParticleBackground.js
@@ -0,0 +1,11 @@
+import Particles from "react-tsparticles"
+import particlescofig from './config/particles-cofing'
+
+const ParticleBackground = () => {
+  return (
+    <Particles params={particlescofig}> </Particles>
+
+  )
+}
+
+export default ParticleBackground
