1824085778040ccdec47a08f124c21dc79c2b37e
onyxgeek <onyxpeek@gmail.com>
2023-05-07 14:45:36 +0300

    feat: add the css and js for the home page

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import Design from './Design'
+
+import '../index.css'
+import { Button } from './Button';
+import './HomeSection.css';
+
 function HomeSection() {
   return (
     <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>Aventurile te asteapta cu noi</h1>
+      <p>Ce mai astepti?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          Alaturate
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomeSection
