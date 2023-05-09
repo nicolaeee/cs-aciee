4c4a35cac97d0a1aece1f20af9906254b5c9af14
onyxgeek <onyxpeek@gmail.com>
2023-05-09 23:15:32 +0300

    feat: add the style for the header

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,8 +7,8 @@ function HomePage() {
       <div className="wrapper">
         <div className="home__wrapper">
           <div className="home-info">
-          <h1 className="home__header">Cine suntem noi?</h1>
-            <p>După cum ne sugerează numele, suntem o adunare de studenți dornici să formăm o comunitate de voluntari care să devină o "voce a studenților".</p>
+          <h1 className="home__header">Cine <span>suntem </span>noi?</h1>
+            <p className="home__description">După cum ne sugerează numele, suntem o adunare de studenți dornici să formăm o comunitate de voluntari care să devină o "voce a studenților".</p>
           </div>
           <div className="home-contact">
             <img src="../images/home_friends.svg"/>
