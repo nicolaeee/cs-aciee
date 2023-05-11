d6aa8dbaa6856595c1ea83a405b2dfe07cb5f481
onyxgeek <onyxpeek@gmail.com>
2023-05-11 20:22:38 +0300

    feat: add the header style for about section

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -7,7 +7,7 @@ function AboutPage() {
     <div className="wrapper">
       <div className="home__wrapper">
         <div className="home-info">
-        <h1 className="home__header__about">Universitatea<span>Dunarea</span>de Jos</h1>
+        <h1 className="home__header__about">Universitatea<span> Dunarea </span>de Jos</h1>
           <p className="home__description">Clubul nostru de studenti face parte din Liga Studentilor din Galati in cadrul Universitatii Dunarea de Jos, suntem multumiti de faptul ca suntem sustinuti de Facultatea de Automatică, Calculatoare, Inginerie Electrică și Electronică</p>
           <Button buttonStyle="btn--outline" buttonSize="btn--large">
             Înscrie-te acum!
