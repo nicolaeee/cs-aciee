a80274d0d0a59501af5e228954feccc081f6ac06
onyxgeek <onyxpeek@gmail.com>
2023-05-11 20:12:05 +0300

    feat: add the about page

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import './About.css'
+import { Button } from '../../components/ButtonComponent/Button';
 function AboutPage() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page of my website.</p>
+    <section className="home">
+    <div className="wrapper">
+      <div className="home__wrapper">
+        <div className="home-info">
+        <h1 className="home__header__about">Universitatea<span>Dunarea</span>de Jos</h1>
+          <p className="home__description"></p>
+          <Button buttonStyle="btn--outline" buttonSize="btn--large">
+            Înscrie-te acum!
+          </Button>
+        </div>
+        <div className="home-contact">
+          <img src="../images/home_friends.svg"/>
+        </div>
+      </div>
     </div>
+  </section>
   );
 }
 
