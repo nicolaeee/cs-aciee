46798f2b119351e512d48470c35c019c9a7b5413
onyxgeek <onyxpeek@gmail.com>
2023-05-11 20:17:38 +0300

    feat: add the about component

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -8,13 +8,13 @@ function AboutPage() {
       <div className="home__wrapper">
         <div className="home-info">
         <h1 className="home__header__about">Universitatea<span>Dunarea</span>de Jos</h1>
-          <p className="home__description"></p>
+          <p className="home__description">Clubul nostru de studenti face parte din Liga Studentilor din Galati in cadrul Universitatii Dunarea de Jos, suntem multumiti de faptul ca suntem sustinuti de Facultatea de Automatică, Calculatoare, Inginerie Electrică și Electronică</p>
           <Button buttonStyle="btn--outline" buttonSize="btn--large">
             Înscrie-te acum!
           </Button>
         </div>
         <div className="home-contact">
-          <img src="../images/home_friends.svg"/>
+          <img src="../images/about.svg"/>
         </div>
       </div>
     </div>
