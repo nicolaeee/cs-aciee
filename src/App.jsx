be2dc49ed8cd6770ad4d7b55a5cc27a514bcdd9c
onyxgeek <onyxpeek@gmail.com>
2023-05-12 09:04:37 +0300

    init: start the contact page

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Background from './components/ParticlesComponent/Background'
 import HomePage from './pages/HomePage/HomePage'
 import About from './pages/AboutPage/AboutPage'
+import ContactPage from './pages/ContactPage/ContactPage'
 
 function App() {
   return (
    <>
    <Router>
-    <Background/>
+    {/* <Background/> */}
     <Navbar/>
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<ContactPage/>} />
     </Routes>
    </Router>
    </>
